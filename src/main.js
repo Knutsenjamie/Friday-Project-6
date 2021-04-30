ed6ae074525b6a23d70d091e887eb3818857c5b7
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-04-30 10:30:21 -0700

    refactor entire project

--- src/main.js
+++ src/main.js
@@ -5,5 +5,31 @@ import './css/styles.css';
 import ExchangeService from './currency-exchange-service.js';
 
 function exchangedRate(response, usDollars, otherCurrencies) {
-  
+  if (response.result != 'success') {
+    $('#exchangedRate').text(`There was an error: ${response.message}`);
+  } else if (!otherCurrencies) {
+    $('#exchangedRate').text('Please select a currency');
+  } else if (usDollars === '') {
+    $('#exchangedRate').text('Please select/enter number in USD');
+  } else if (response.result === 'success') {
+    $('#exchangedRate').text(`${usDollars} in ${otherCurrencies}: ${response.conversion_rates[otherCurrencies]*usDollars}`); 
+  } else {
+    $('#exchangedRate').text(`There was an error. Please try again.`);
+  }
 }
+
+$(document).ready(function() {
+  $('#showExchangeRate').click(function(event) {
+    event.preventDefault();
+
+    let usDollars = $('#usDollars').val();
+    console.log(usDollars);
+    $('#usDollars').val("");
+    let otherCurrencies = $('input:radio:checked').val();
+    console.log(otherCurrencies);
+    ExchangeService.exchangedRate()
+      .then(function(response) {
+        exchangedRate(response, usDollars, otherCurrencies);
+      });
+  });
+});
